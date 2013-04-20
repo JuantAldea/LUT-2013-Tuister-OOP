a31a1c56ee87bb6497b56c9a9b1c0c6e363717de
Pablo Caro Martín <pablocaro90@gmail.com>
2013-04-20 14:45:01 +0300

    asdf

--- Tuister/src/client/ClientController.java
+++ Tuister/src/client/ClientController.java
@@ -6,15 +6,25 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.channels.ClosedChannelException;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
 
 import javax.xml.bind.JAXBException;
 
+import common.FollowPDU;
+import common.FollowingUsersRequestPDU;
+import common.LikePDU;
 import common.LoginPDU;
+import common.PublishPDU;
 import common.RegisterPDU;
+import common.UnfollowPDU;
+import common.UserContentRequestPDU;
+import common.UserListRequestPDU;
 
-public class ClientController implements Runnable {
+public class ClientController implements Runnable{
 	private String host = "127.0.0.1";
 	private int port = 1234;
 	
@@ -22,40 +32,21 @@ public class ClientController implements Runnable {
 	private Selector selector = null;
 	
 	private GUI gui = null;
-	private ClientModel client = null;
+	private ClientModel model = null;
+	private boolean active = true;
 	
 	public ClientController(){
 		this.gui = new GUI(this);
-		this.gui.run();
 		
-		this.client = new ClientModel();
+		this.model = new ClientModel();
 		
 		try {
 			this.socket = SocketChannel.open(new InetSocketAddress(host, port));
 			this.socket.configureBlocking(false);
+			this.selector = Selector.open();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-	}
-
-	public void processInput(String input) {
-		String[] inputSplit = input.split(" ");
-		
-		if (inputSplit[0] == "register"){
-			if (inputSplit.length == 3){
-				this.register(inputSplit[1], inputSplit[2]);
-			} else {
-				this.gui.printHelp();
-			}
-		} else if (inputSplit[0] == "login"){
-			if (inputSplit.length == 3){
-				this.login(inputSplit[1], inputSplit[2]);
-			} else {
-				this.gui.printHelp();
-			}
-		} else if (inputSplit[0] == "exit"){
-			this.gui.deactivate();
-			// TODO Exit
+			this.exit();
 		}
 	}
 
@@ -69,9 +60,9 @@ public class ClientController implements Runnable {
 		}
 	}
 	
-	public void login(String user, String password) {
+	public void login(String username, String password) {
 		try {
-			this.sendToServer(new LoginPDU(user, password).toXML());
+			this.sendToServer(new LoginPDU(username, password).toXML());
 		} catch (JAXBException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -79,8 +70,95 @@ public class ClientController implements Runnable {
 		}
 	}
 	
+	public void publish(String text) {
+		try {
+			this.sendToServer(new PublishPDU(text).toXML());
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void follow(String username) {
+		try {
+			this.sendToServer(new FollowPDU(username).toXML());
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void unfollow(String username) {
+		try {
+			this.sendToServer(new UnfollowPDU(username).toXML());
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void like(String string) {
+		try {
+			this.sendToServer(new LikePDU(this.model.postID(string)).toXML());
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void unlike(String string) {
+		try {
+			this.sendToServer(new LikePDU(this.model.postID(string)).toXML());
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void following() {
+		try {
+			this.sendToServer(new FollowingUsersRequestPDU().toXML());
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void userContent(String username) {
+		try {
+			this.sendToServer(new UserContentRequestPDU(username).toXML());
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void listUsers() {
+		try {
+			this.sendToServer(new UserListRequestPDU().toXML());
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void exit() {
+		// TODO Exit
+		this.gui.deactivate();
+		this.active = false;
+		this.selector.wakeup();
+	}
+	
 	private void sendToServer(String string) throws IOException {
-		System.out.println("enviando " + string);
 		ByteBuffer buffer = ByteBuffer.allocate(string.length()).order(ByteOrder.BIG_ENDIAN);
 		buffer.clear();
 		buffer.put(string.getBytes());
@@ -88,25 +166,66 @@ public class ClientController implements Runnable {
 		while(buffer.hasRemaining()){
 			this.socket.write(buffer);
 		}
-		System.out.println("enviado");
+	}
+	
+	public void wakeUp() { // TODO useless
+		this.selector.wakeup();
 	}
 
-	public void exit() {
-		this.gui.deactivate();
-	}
-	
-	public void wakeUp() {
-		
-	}
-	
 	@Override
 	public void run() {
+		ByteBuffer buffer = null;
 		
-		
+		while (this.active){
+			try {
+				buffer = ByteBuffer.allocate(this.socket.socket().getReceiveBufferSize()).order(ByteOrder.BIG_ENDIAN);
+				this.socket.register(selector, SelectionKey.OP_READ);
+				selector.select();
+			} catch (ClosedChannelException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	        Iterator<SelectionKey> it = selector.selectedKeys().iterator();
+	        
+	        while (it.hasNext()) {
+	        	SelectionKey selKey = it.next();
+	            it.remove();
+	            if (selKey.isReadable()) {
+	            	SocketChannel s = (SocketChannel) selKey.channel();
+	            	int r = 0;
+	                try {
+						r = s.read(buffer);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+	                
+	                if (r <= 0){
+	                	this.exit();
+	                } else {
+	                	this.model.processData(buffer);
+	                }
+	            }
+	        }
+		}
 	}
 	
 	public static void main(String[] args) {
 		ClientController controller = new ClientController();
-		controller.run();
+		Thread guiThread = new Thread(controller.gui);
+		Thread controllerThread = new Thread(controller);
+		
+		guiThread.start();
+		controllerThread.start();
+		
+		try {
+			guiThread.join();
+			controllerThread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("End of potato");
 	}
+
 }
--- Tuister/src/client/ClientModel.java
+++ Tuister/src/client/ClientModel.java
@@ -1,8 +1,22 @@
 package client;
 
+import java.nio.ByteBuffer;
+
 public class ClientModel {
 	
 	public ClientModel() {
         
     }
+
+	public Integer postID(String string) {
+		return 1;
+	}
+
+	public void processData(ByteBuffer buffer) {
+		byte[] byteArray = new byte[buffer.remaining()];
+		buffer.get(byteArray);
+		String s = new String(byteArray);
+		
+		System.out.println(s);
+	}
 }
