1def47860854e2d16587ed30a4f9ea2337ed9901
Juan Antonio Aldea Armenteros <cyberjuen@gmail.com>
2013-04-21 15:20:59 +0300

    Nesting reduced

--- Tuister/src/database/DatabaseWrapper.java
+++ Tuister/src/database/DatabaseWrapper.java
@@ -42,24 +42,25 @@ public class DatabaseWrapper {
         System.out.println("Connection: " + connection);
     }
 
-    public Integer registerUser(String username, String password) {
+    public boolean registerUser(String username, String password) {
         Statement statement;
         try {
             statement = connection.createStatement();
             statement.setQueryTimeout(30);
-            ResultSet rs = statement.executeQuery(String.format("select id from users where username = \"%s\"", username));
-
-            if (!rs.next()) {
-                statement.executeUpdate(String.format("insert into users(username, password) values(\"%s\", \"%s\")", username, password));
-                rs = statement.executeQuery(String.format("select id from users where username = \"%s\"", username));
-                return rs.getInt("id");
+            // check if there is already a user with such username
+            ResultSet queryResults = statement.executeQuery(String.format("select id from users where username = \"%s\"", username));
+            // results => username already taken
+            if (queryResults.next()) {
+                return false;
             }
+            // !results => register new user
+            statement.executeUpdate(String.format("insert into users(username, password) values(\"%s\", \"%s\")", username, password));
+            queryResults = statement.executeQuery(String.format("select id from users where username = \"%s\"", username));
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
+            return false;
         }
-        return -1;
-
+        return true;
     }
 
     public Integer login(String username, String password) {
@@ -67,133 +68,148 @@ public class DatabaseWrapper {
         try {
             statement = connection.createStatement();
             statement.setQueryTimeout(30);
-            ResultSet rs = statement.executeQuery(String.format("select id from users where username = \"%s\" AND password=\"%s\"", username,
-                    password));
-            if (rs.next()) {
-                return rs.getInt("id");
+            // check if there is a user witch such username and password
+            ResultSet queryResults = statement.executeQuery(String.format("select id from users where username = \"%s\" AND password=\"%s\"",
+                    username, password));
+            // results => valid login data, return the userID
+            if (queryResults.next()) {
+                return queryResults.getInt("id");
             }
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
+            return -1;
         }
         return -1;
     }
 
-    public void publish(Integer userID, String text) {
+    public boolean publish(Integer userID, String text) {
         Statement statement;
         try {
             statement = connection.createStatement();
             statement.setQueryTimeout(30);
-            int result = statement.executeUpdate(String.format("insert into posts(author, body) values (\"%d\", \"%s\")", userID, text));
-            if (result == 1) {
-                System.out.println("TODO BIEN");
-            } else {
-                // errror
-            }
+            statement.executeUpdate(String.format("insert into posts(author, body) values (%d, \"%s\")", userID, text));
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
+            return false;
         }
+        return true;
     }
 
-    public void like(Integer userID, Integer postID) {
+    public Boolean like(Integer userID, Integer postID) {
         Statement statement;
         try {
             statement = connection.createStatement();
             statement.setQueryTimeout(30);
-            ResultSet rs = statement.executeQuery(String.format("select * from likes where user=\"%d\" and post=\"%s\"", userID, postID));
-            if (!rs.next()) {
-                int result = statement.executeUpdate(String.format("insert into likes(user, post) values (\"%d\", \"%s\")", userID, postID));
-                if (result == 1) {
-                    System.out.println("TODO BIEN");
-                }
-                rs = statement.executeQuery(String.format("select likes from posts where id=\"%d\"", postID));
-                rs.next();
-                result = statement.executeUpdate(String.format("update posts set likes=%d where id =%d", rs.getInt("likes") + 1, postID));
+
+            // check if the post exists
+            ResultSet querryResults = statement.executeQuery(String.format("select * from posts where id=%d", postID));
+            // no results => invalid post
+            if (!querryResults.next()) {
+                return false;
             }
+
+            // results => user already likes this post
+            querryResults = statement.executeQuery(String.format("select * from likes where user=%d and post=%d", userID, postID));
+            if (querryResults.next()) {
+                return true;
+            }
+
+            // no results => it's a new like, like it and update the post's likes count
+            statement.executeUpdate(String.format("insert into likes(user, post) values (%d, %d)", userID, postID));
+            querryResults = statement.executeQuery(String.format("select likes from posts where id=%d", postID));
+            statement.executeUpdate(String.format("update posts set likes=%d where id =%d", querryResults.getInt("likes") + 1, postID));
+
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
+            return false;
         }
+        return true;
     }
 
-    public void unLike(Integer userID, Integer postID) {
+    public Boolean unLike(Integer userID, Integer postID) {
         Statement statement;
         try {
             statement = connection.createStatement();
             statement.setQueryTimeout(30);
-            ResultSet rs = statement.executeQuery(String.format("select * from likes where user=\"%d\" and post=\"%s\"", userID, postID));
-            if (rs.next()) {
-                int result = statement.executeUpdate(String.format("delete from likes where user=\"%d\" and post =\"%d\"", userID, postID));
-                if (result == 1) {
-                    System.out.println("TODO BIEN");
-                }
-                rs = statement.executeQuery(String.format("select likes from posts where id=\"%d\"", postID));
-                rs.next();
-                result = statement.executeUpdate(String.format("update posts set likes=%d where id =%d", rs.getInt("likes") - 1, postID));
+            // no results => user doesn't like this post
+            ResultSet queryResults = statement.executeQuery(String.format("select * from likes where user=\"%d\" and post=\"%s\"", userID, postID));
+            if (!queryResults.next()) {
+                return true;
             }
+            // results => delete the like and decrease post's likes count
+            statement.executeUpdate(String.format("delete from likes where user=\"%d\" and post =\"%d\"", userID, postID));
+            // get the post
+            queryResults = statement.executeQuery(String.format("select likes from posts where id=\"%d\"", postID));
+            queryResults.next();
+            // decrease the counter
+            statement.executeUpdate(String.format("update posts set likes=%d where id =%d", queryResults.getInt("likes") - 1, postID));
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
+            return false;
         }
+        return true;
     }
 
-    public void follow(Integer userID, String userToFollow) {
+    public boolean follow(Integer userID, String userToFollow) {
         Statement statement;
         try {
             statement = connection.createStatement();
             statement.setQueryTimeout(30);
-            ResultSet rs = statement.executeQuery(String.format("select id from users where username=\"%s\"", userToFollow));
-            // valid user
-            if (rs.next()) {
-                Integer userToFollowID = rs.getInt("id");
-                rs = statement.executeQuery(String.format("select * from followers where follower=%d and followed=%d", userID, userToFollowID));
-                // not following already
-                if (!rs.next()) {
-                    int result = statement.executeUpdate(String.format("insert into followers(follower, followed) values(%d, %d)", userID,
-                            userToFollowID));
-                    // send ack
-                } else {
-                    // already following
-                }
-            } else {
-                // error user invalid
+            // check if target user is valid
+            ResultSet queryResults = statement.executeQuery(String.format("select id from users where username=\"%s\"", userToFollow));
+            // no results => invalid user
+            if (!queryResults.next()) {
+                return false;
             }
+            // check if user already follows target user
+            Integer userToFollowID = queryResults.getInt("id");
+            // cannot follow yourself
+            if (userID == userToFollowID) {
+                return false;
+            }
+            queryResults = statement.executeQuery(String.format("select * from followers where follower=%d and followed=%d", userID, userToFollowID));
+            // results => already following
+            if (queryResults.next()) {
+                return true;
+            }
+            statement.executeUpdate(String.format("insert into followers(follower, followed) values(%d, %d)", userID, userToFollowID));
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
+            return false;
         }
+        return true;
     }
 
-    public void unFollow(Integer userID, String userToFollow) {
+    public boolean unFollow(Integer userID, String userToUnFollow) {
         Statement statement;
         try {
             statement = connection.createStatement();
             statement.setQueryTimeout(30);
-            ResultSet rs = statement.executeQuery(String.format("select id from users where username=\"%s\"", userToFollow));
-            // valid user
-            if (rs.next()) {
-                Integer userToFollowID = rs.getInt("id");
-                if (userID != userToFollowID) {
-                    rs = statement.executeQuery(String.format("select * from followers where follower=%d and followed=%d", userID, userToFollowID));
-                    // not following already
-                    if (rs.next()) {
-                        int result = statement.executeUpdate(String.format("delete from followers where follower=%d and followed=%d", userID,
-                                userToFollowID));
-                        // send ack
-                    } else {
-                        // already following
-                    }
-                } else {
-                    // cannot follow yourself
-                }
-            } else {
-                // error user invalid
+            // check if target user is valid
+            ResultSet queryResults = statement.executeQuery(String.format("select id from users where username=\"%s\"", userToUnFollow));
+            // no results => invalid target user
+            if (!queryResults.next()) {
+                return false;
             }
+            // get target user ID
+            Integer userToUnFollowID = queryResults.getInt("id");
+            // cannot unfollow yourself
+            if (userID == userToUnFollowID) {
+                return false;
+            }
+            queryResults = statement.executeQuery(String
+                    .format("select * from followers where follower=%d and followed=%d", userID, userToUnFollowID));
+            // already not following
+            if (!queryResults.next()) {
+                return true;
+            }
+            statement.executeUpdate(String.format("delete from followers where follower=%d and followed=%d", userID, userToUnFollowID));
+
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
+            return false;
         }
+        return true;
     }
 
     public ResultSet followingUsersRequest(Integer userID) {
@@ -223,41 +239,43 @@ public class DatabaseWrapper {
 
     public ResultSet userContentRequest(String username) {
         Statement statement;
-        ResultSet rs = null;
+        ResultSet queryResults = null;
         try {
             statement = connection.createStatement();
             statement.setQueryTimeout(30);
-            rs = statement.executeQuery(String.format("select id from users where username=\"%s\"", username));
-            // valid user
-            if (rs.next()) {
-                rs = statement.executeQuery(String.format("select * from posts where author=%d order by posts.date asc", rs.getInt("id")));
-            } else {
-                rs = null;
+            // check if requested username is valid
+            queryResults = statement.executeQuery(String.format("select id from users where username=\"%s\"", username));
+            // no results => invalid user
+            if (!queryResults.next()) {
+                return null;
             }
+            // results => get user's posts
+            queryResults = statement.executeQuery(String.format("select * from posts where author=%d order by posts.date asc",
+                    queryResults.getInt("id")));
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
+            return null;
         }
-        return rs;
+        return queryResults;
     }
 
     public ResultSet update(Integer userID) {
         Statement statement;
-        ResultSet rs = null;
-
+        ResultSet queryResults = null;
         try {
             statement = connection.createStatement();
             statement.setQueryTimeout(30);
-
-            rs = statement.executeQuery(String.format("select posts.body, posts.likes, users.username, posts.post_date, posts.id from posts, followers, users where "
-                    + "(posts.author = 1 or (posts.author = followers.followed and followers.follower = 1)) and users.id = posts.author " +
-                    "order by post_date asc", userID,
-                    userID));
+            // get feed from followed users and user's posts
+            queryResults = statement.executeQuery(String.format(
+                    "select posts.body, posts.likes, users.username, posts.post_date, posts.id from posts, followers, users where "
+                            + "(posts.author = 1 or (posts.author = followers.followed and followers.follower = 1)) and users.id = posts.author "
+                            + "order by post_date asc", userID, userID));
 
         } catch (SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
+            return null;
         }
-        return rs;
+        return queryResults;
     }
 }
